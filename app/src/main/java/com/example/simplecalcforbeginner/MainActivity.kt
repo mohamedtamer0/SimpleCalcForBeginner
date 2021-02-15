b4525bace5d8472ab27eaff641267e730da0a5ed
mohamed tamer0 <mt4452242@gmail.com>
2021-02-15 21:37:22 +0200

    update

--- app/src/main/java/com/example/simplecalcforbeginner/MainActivity.kt
+++ app/src/main/java/com/example/simplecalcforbeginner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecalcforbeginner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,33 @@ class MainActivity : AppCompatActivity() {
 
         var n1 = 0
         var n2 = 0
-
+        var str = "Result = "
         fun setNum() {
             n1 = txt1.text.toString().toInt()
             n2 = txt2.text.toString().toInt()
         }
 
+
+        btnAdd.setOnClickListener {
+            setNum(); txtR.text = (n1 + n2).toString()
+            var add = str + txtR.text.toString()
+            Toast.makeText(this@MainActivity, add, Toast.LENGTH_SHORT).show()
+        }
+        btnSub.setOnClickListener {
+            setNum(); txtR.text = (n1 - n2).toString()
+            var sub = str + txtR.text.toString()
+            Toast.makeText(this@MainActivity, sub, Toast.LENGTH_SHORT).show()
+        }
+        btnMulti.setOnClickListener {
+            setNum(); txtR.text = (n1 * n2).toString()
+            var multi = str + txtR.text.toString()
+            Toast.makeText(this@MainActivity, multi, Toast.LENGTH_SHORT).show()
+        }
+        btnDiv.setOnClickListener {
+            setNum(); txtR.text = (n1 / n2).toString()
+            var div = str + txtR.text.toString()
+            Toast.makeText(this@MainActivity, div, Toast.LENGTH_SHORT).show()
+        }
 
 
 
